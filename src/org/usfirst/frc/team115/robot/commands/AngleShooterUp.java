b5d611b2724c5dbfd8884c66e71cb294098ae62a
BenCuan <ben.cuan@gmail.com>
2016-01-28 17:11:06 -0800

    Getting ready for PR

---
+++ src/org/usfirst/frc/team115/robot/commands/AngleShooterUp.java
@@ -0,0 +1,44 @@
+package org.usfirst.frc.team115.robot.commands;
+
+import org.usfirst.frc.team115.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class AngleShooterUp extends Command {
+
+    public AngleShooterUp() {
+        
+    }
+    
+    public double getRamp(double percentToTarget) {
+		return ((1 - 0.70 / (1 + 12000*Math.exp(-0.115 * percentToTarget))));
+	}
+    
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.shooterAngler.goUp();
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	Robot.shooterAngler.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
