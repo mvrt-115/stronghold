f04ea65472b359019c3e3b3c2902beacb55b895a
BenCuan <ben.cuan@gmail.com>
2016-01-28 17:16:42 -0800

    Getting ready for PR

---
+++ src/org/usfirst/frc/team115/robot/OI.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team115.robot;
+
+import org.usfirst.frc.team115.robot.commands.AngleShooterDown;
+import org.usfirst.frc.team115.robot.commands.AngleShooterStop;
+import org.usfirst.frc.team115.robot.commands.AngleShooterUp;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+	private Joystick joystick;
+	private JoystickButton angleUp;
+	private JoystickButton angleDown;
+	
+	public OI() {
+		joystick = new Joystick(RobotMap.JOYSTICK);
+		
+		angleUp = new JoystickButton(joystick, RobotMap.BUTTON_ANGLE_UP);
+		angleDown = new JoystickButton(joystick, RobotMap.BUTTON_ANGLE_DOWN);
+		
+		angleUp.whenPressed(new AngleShooterUp());
+		angleUp.whenReleased(new AngleShooterStop());
+		
+		angleDown.whenPressed(new AngleShooterDown());
+		angleDown.whenReleased(new AngleShooterStop());
+		
+	}
+	
+    public Joystick getJoystick(){
+    	return joystick;
+    }
+}
+
---
+++ src/org/usfirst/frc/team115/robot/RobotMap.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team115.robot;
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+	
+	//DriveTrain
+	public static final int FRONT_LEFT_MOTOR = 0;
+	public static final int FRONT_RIGHT_MOTOR = 1;
+	public static final int BACK_LEFT_MOTOR = 2;
+	public static final int BACK_RIGHT_MOTOR = 3;
+	
+	//Joystick ID
+	public static final int JOYSTICK = 0;
+	
+	//DriveStraight
+	public static final int ENCODER_SCALE = 1444; //TODO change number if necessary
+	public static final double DRIVE_OUTPUT_RANGE = 0.6;
+	public static final double DRIVE_SETPOINT = 1; //TODO
+	
+	//Shooter Angler
+	public static final int SHOOTER_ANGLER_LEFT = 1;
+	public static final int SHOOTER_ANGLER_RIGHT = 2;
+	public static final double SHOOTER_ANGLER_SPEED = 0.5; //TODO
+	public static final int ENCODER_ANGLER_A = 1;
+	public static final int ENCODER_ANGLER_B = 2;
+	public static final double ENCODER_ONE_DEGREE = 1024/360;
+	public static final int BUTTON_ANGLE_UP = 1;
+	public static final int BUTTON_ANGLE_DOWN = 2;
+	public static final double BOTTOM_HEIGHT = 0;
+	
+	//Shooter Intake
+	public static final int SHOOTER_INTAKE_TOP = 1;
+	public static final int SHOOTER_INTAKE_BOTTOM = 2;
+	public static final double SHOOTER_INTAKE_SPEED = 0.5; //TODO 
+	
+}
