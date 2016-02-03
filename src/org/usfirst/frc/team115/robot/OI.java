f1cf2726c895a6ac364c9418afd7e72002c1aba8
Dohyun Kim <dohyunk314@gmail.com>
2016-02-02 19:56:12 -0800

    deleted errors

--- src/org/usfirst/frc/team115/robot/OI.java
+++ src/org/usfirst/frc/team115/robot/OI.java
@@ -1,30 +1,13 @@
 package org.usfirst.frc.team115.robot;
 
-<<<<<<< HEAD
 import edu.wpi.first.wpilibj.Joystick;
-=======
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
->>>>>>> 281f4ef0ec7c1724f76b2edc51b4888498674a83
 
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
 public class OI {
-<<<<<<< HEAD
-	
-	private Joystick joystick;
-	
-	public OI() {
-		joystick = new Joystick(RobotMap.JOYSTICK);
-	}
-	
-    public Joystick getJoystick() {
-    	return joystick;
-    }
-=======
+
    Joystick joystick;
    
    public OI()
@@ -36,6 +19,5 @@ public class OI {
    {
 	   return joystick;
    }
->>>>>>> 281f4ef0ec7c1724f76b2edc51b4888498674a83
 }
 
