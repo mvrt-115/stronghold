e46fc0eadb3658a4b10fd810d7a3a604756e5b04
Eric Wang <erwang01@gmail.com>
2016-02-10 16:55:08 -0800

    StopShooter Command

---
+++ src/org/usfirst/frc/team115/robot/commands/StopShooter.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc.team115.robot.commands;
+
+import org.usfirst.frc.team115.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ * 
+ * 
+ * @author Eric Wang
+ */
+
+
+public class StopShooter extends Command{
+	
+	public StopShooter() {
+		requires(Robot.shooter);
+	}
+
+	@Override
+	protected void initialize() {
+		
+	}
+
+	@Override
+	protected void execute() {
+		Robot.shooter.stop();
+		Robot.shooter.retract();
+	}
+
+	@Override
+	protected boolean isFinished() {
+		return true;
+	}
+
+	@Override
+	protected void end() {
+		Robot.shooter.stop();
+		Robot.shooter.retract();
+		
+	}
+
+	@Override
+	protected void interrupted() {
+		end();
+		
+	}
+
+}
