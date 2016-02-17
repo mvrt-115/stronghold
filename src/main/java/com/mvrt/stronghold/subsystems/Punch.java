b42e4eee78663f4ded952fd6c0e0eb4d67de35e7
Michael Sandler <jokegred@gmail.com>
2016-02-16 23:58:00 -0800

    Add the punch subsystem

---
+++ src/main/java/com/mvrt/stronghold/subsystems/Punch.java
@@ -0,0 +1,35 @@
+package com.mvrt.stronghold.subsystems;
+
+import com.mvrt.stronghold.Constants;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ * Created by Michael Sandler on 2/16/16.
+ */
+public class Punch extends Subsystem {
+
+  private DoubleSolenoid punch;
+
+  public Punch() {
+    punch  = new DoubleSolenoid(Constants.kPunchSolenoidA, Constants.kPunchSolenoidB);
+  }
+
+  public void extend() {
+    punch.set(Value.kForward);
+  }
+
+  public void retract() {
+    punch.set(Value.kReverse);
+  }
+
+  public boolean isExtended() {
+    return punch.get().equals(Value.kForward);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+
+  }
+}
