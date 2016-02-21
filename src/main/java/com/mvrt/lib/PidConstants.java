eabeb705b10a3b906a62ded8dd7b73cf9d86d229
Heather Baker <heather@heatherbaker.com>
2016-02-20 17:53:00 -0800

    Code for inversion and percision drive
    Added code in austin drive for inversion and precision. Created arcade
    drive with same parameters.

--- src/main/java/com/mvrt/lib/PidConstants.java
+++ src/main/java/com/mvrt/lib/PidConstants.java
@@ -1,14 +1,14 @@
 package com.mvrt.lib;
 
 /**
- * Created by lee on 2/17/16.
- */
+* Created by lee on 2/17/16.
+*/
 public class PidConstants {
-    public final double kP, kI, kD;
+  public final double kP, kI, kD;
 
-    public PidConstants(double kP, double kI, double kD) {
-      this.kP = kP;
-      this.kI = kI;
-      this.kD = kD;
-    }
+  public PidConstants(double kP, double kI, double kD) {
+    this.kP = kP;
+    this.kI = kI;
+    this.kD = kD;
+  }
 }
