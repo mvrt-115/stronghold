f4d6931a1636c0225f7d24780ab9bce9488e0d68
Heather Baker <heather@heatherbaker.com>
2016-02-21 11:28:15 -0800

    Add method to get presets
    Added a method in operatorInterface to get presets for the angler based
    off of an axis from the operatorJoystick.

--- src/main/java/com/mvrt/stronghold/OperatorInterface.java
+++ src/main/java/com/mvrt/stronghold/OperatorInterface.java
@@ -19,26 +19,31 @@ public class OperatorInterface {
     operatorJoystick = new Joystick(Constants.kOperatorJoystick);
 
     // intake with the flywheels
-    new JoystickButton(operatorJoystick, Constants.kIntakeButton).
-        whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterIntake));
-    new JoystickButton(operatorJoystick, Constants.kIntakeButton).
-        whileActive(new SetFlywheelSpeed(Robot.rightFlywheel, Constants.kShooterIntake));
+    new JoystickButton(operatorJoystick, Constants.kIntakeButton)
+        .whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterIntake));
+    new JoystickButton(operatorJoystick, Constants.kIntakeButton)
+        .whileActive(new SetFlywheelSpeed(Robot.rightFlywheel, Constants.kShooterIntake));
 
     // run one preset of the flywheels
-    new JoystickButton(operatorJoystick, Constants.kShootPresetButton1).
-        whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterPreset1));
+    new JoystickButton(operatorJoystick, Constants.kShootPresetButton1)
+        .whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterPreset1));
     new JoystickButton(operatorJoystick, Constants.kShootPresetButton1)
         .whileActive(new SetFlywheelSpeed(Robot.rightFlywheel, Constants.kShooterPreset1));
 
     // run another preset of the flywheels
-    new JoystickButton(operatorJoystick, Constants.kShootPresetButton2).
-        whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterPreset2));
-    new JoystickButton(operatorJoystick, Constants.kShootPresetButton2).
-        whileActive(new SetFlywheelSpeed(Robot.rightFlywheel, Constants.kShooterPreset2));
+    new JoystickButton(operatorJoystick, Constants.kShootPresetButton2)
+        .whileActive(new SetFlywheelSpeed(Robot.leftFlywheel, Constants.kShooterPreset2));
+    new JoystickButton(operatorJoystick, Constants.kShootPresetButton2)
+        .whileActive(new SetFlywheelSpeed(Robot.rightFlywheel, Constants.kShooterPreset2));
 
     // shoot the ball
     new JoystickButton(operatorJoystick, Constants.kExtendPistonButton).whenPressed(new Shoot());
 
+  }
+  
+  public double getAnglerPreset() {
+    double location = operatorJoystick.getThrottle(); // TODO get the correct axis
+    return Constants.kAnglerPresets[(int)(location * (Constants.kAnglerPresets.length - 1))];
   }
 
   public Joystick getDriveJoystick() {
